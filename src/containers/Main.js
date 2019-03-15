89509fb6d8335a8a157dff846fff7ad2ed67829b
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-03-15 11:48:13 -0300

    Wired up client side AuthForm

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Homepage from "../components/Homepage";
+import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
+import { authUser } from '../store/actions/auth';
 
 const Main = props => {
+  const { authUser } = props;
   return (
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route exact path="/signin" render={props => {
+          return (
+            <AuthForm
+              onAuth={authUser}
+              buttonText="Log In"
+              heading="Welcome Back."
+              {...props} />
+          );
+        }}/>
+        <Route exact path="/signup" render={props => {
+          return (
+            <AuthForm
+              onAuth={authUser}
+              signUp
+              buttonText="Sign Me Up!"
+              heading="Join Twitta today."
+              {...props} />
+          );
+        }}/>
       </Switch>
     </div>
   );
@@ -19,4 +41,4 @@ const mapStateToProps = state => {
   };
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, { authUser })(Main));
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+/*
+  A wrapper around axios API call that formats errors, etc
+  @param {string} method - the http verb we want to use
+  @param {string} path - the route path / endpoint
+  @param {object} data = (optional) data in JSON form for POST requests
+*/
+const apiCall = (method, path, data) => {
+  return new Promise((resolve, reject) => {
+    return axios[method](path, data)
+      .then(res => {
+        return resolve(res.data);
+    }).catch(err => {
+        return reject(err.response.data.error);
+    })
+  })
+}
+
+export default apiCall;
